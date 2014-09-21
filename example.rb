576b63412f47308067ee01fadcbb4214f7b2b2fc
cfi30 <git@cfillion.tk>
2014-09-21 03:24:32 -0400

    beautify example.rb

--- example.rb
+++ example.rb
@@ -5,23 +5,20 @@ end
 
 adv = BlackList.new 'adv'
 test = BlackList.new 'test'
-trackers = BlackList.new 'tracker'
+tracker = BlackList.new 'tracker'
 
-blocked = [adv, test, trackers]
+blocked = [adv, test, tracker]
 
 define_action :block do
   replace_by 'http://webfilter.net/block_page'
+
   # intercept do |req, res|
   #   res.write "This webpage is blocked!"
   # end
 end
 
 run proc {|uri, extras|
-  if 'perdu.com' == uri.host
-    replace_by 'http://google.com'
-  end
+  redirect_to 'http://google.com' if uri.host == 'perdu.com'
 
-  if blocked.any? {|bl| bl.include? uri }
-    block
-  end
+  block if blocked.any? {|bl| bl.include? uri }
 }
